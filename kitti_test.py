a4d51998262d34c928e9db61acfdc8e36b7a7333
loaywael <hythamconn1@gmail.com>
2022-01-15 20:26:07 +0200

    Changes to be committed: 	modified:   kitti_test.py: adopted the new visualization class 	new file:   output/kitti-orb-1000-mono-visual_odom.npz: kitti sequence<3> test scene 	modified:   visualization.py: added a new visualization class

--- kitti_test.py
+++ kitti_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from visualization import Interactive3DVisualizer
+from visualization import Scenes3DVisualizer
 from visualization import set_pointcloud_obj
 from visualization import set_camera_view
 from odom import SiftOdom, OrbOdom
@@ -25,21 +25,20 @@ cv_win_title = f"{args.data}-{args.camera}-{args.model}"
 kitti_src = "/home/loay/Documents/datasets/kitti/raw/odometry_gray/dataset"
 kitti_odom = pykitti.odometry(kitti_src, "%02d"%args.vid_seq)
 
-cam0 = Camera(*(1226, 370), kitti_odom.calib.P_rect_00)
+cam0 = Camera(*(960, 290), kitti_odom.calib.P_rect_00)
 odom_params = (cam0.K, args.nbr_features)
 model = OrbOdom(*odom_params) if args.model == 'orb' else SiftOdom(*odom_params)
 img_to_pcd_tf = [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]
 gt_path = np.array([pose[:3, 3] for pose in kitti_odom.poses])
 
-pcd_gt_path = o3d.geometry.PointCloud()
-pcd_pred_path = o3d.geometry.PointCloud()
-cam_view_mesh = o3d.geometry.LineSet()
+pcd_gt_path = o3d.t.geometry.PointCloud()
+pcd_pred_path = o3d.t.geometry.PointCloud()
+cam_view_mesh = o3d.t.geometry.LineSet()
 
 set_pointcloud_obj(pcd_gt_path, np.array(gt_path), color=[0, 0, 1], tf=img_to_pcd_tf)
-vis3d = Interactive3DVisualizer("Odom", (0, 960), (960, 1080))
-vis3d.add_geometries([pcd_gt_path, pcd_pred_path, cam_view_mesh])
+vis3d = Scenes3DVisualizer("Odom")
+vis3d.add_geometries([{"name":"gt_path", "geometry": pcd_gt_path}])
 cv2.namedWindow(cv_win_title)
-cv2.moveWindow(cv_win_title, 0, 0)
 
 
 def main(args):
@@ -48,13 +47,18 @@ def main(args):
             frame = np.asarray(img)
             try:
                 model.track_motion(frame)
+                frame = cv2.resize(frame, (960, 290))
                 set_pointcloud_obj(pcd_pred_path, model.trajectory, color=[1, 0, 0], tf=img_to_pcd_tf)
                 cam0.setProjectionMtx(model.cam_tf[-1][:3])         # last TF -> P(3x4)
                 set_camera_view(cam_view_mesh, cam0.view(20), tf=img_to_pcd_tf)
-                vis3d.show(pcd_pred_path)
-                vis3d.show(cam_view_mesh)
+                vis3d.add_geometries([
+                    {"name":"pred_path", "geometry": pcd_pred_path},
+                    {"name":"cam_view", "geometry": cam_view_mesh},
+                ])
+                vis3d.show(interactive=False, line_width=3, point_size=3)
             except Exception as e: print(e)
             cv2.imshow(cv_win_title, frame)
+            cv2.moveWindow(cv_win_title, 0, 0)
             if cv2.waitKey(1) == ord('q'): break
     except KeyboardInterrupt: pass
     if args.save:
--- visualization.py
+++ visualization.py
@@ -2,8 +2,57 @@
 from camera import Camera
 import open3d as o3d
 import numpy as np
+import threading
 import pykitti
 import time
+
+
+class Scenes3DVisualizer:
+    def __init__(self, win_title="Open3D", win_size=(1920, 1080),
+        show_axes=True, show_ground=True) -> None:
+        '''Scene Constructor'''
+        self._app = o3d.visualization.gui.Application.instance
+        self._app.initialize()
+        self._win = o3d.visualization.O3DVisualizer(
+            title=win_title, width=win_size[0], height=win_size[1],
+        )
+        self._win.show_skybox(False)
+        self._win.show_menu(False)
+        self._win.set_background(np.float32([0]*4), None)
+        self._win.show_ground = show_ground
+        self._app.add_window(self._win)
+        self._is_view_initialized = False
+
+    def add_geometries(self, geometries):
+        '''Updates the scene'''
+        for geometry in geometries:
+            self._win.remove_geometry(geometry["name"])
+            self._win.add_geometry(geometry)
+        
+    def show(self, line_width=3, point_size=3, 
+        save=False, filename=None, interactive=False):
+        '''View the drawn scene'''
+        # self._win.show(True)
+        self._win.post_redraw()
+        self._win.line_width = line_width
+        self._win.point_size = point_size
+        if save: 
+            if filename is None: file = "/output/tmp.png"
+            self._win.capture_screen_image(filename)
+        if interactive: self._app.run()
+        else: self._app.run_one_tick()
+        if not self._is_view_initialized: 
+            self._win.reset_camera_to_default()
+            self._is_view_initialized = True
+
+    def close(self):
+        self._win.close()
+        self._app.quit()
+
+    # >>>>>>>> proposed update_view_method
+    # bounds = self._win.scene.bounding_box
+    # self._win.setup_camera(60.0, bounds, bounds.get_center())
+    # self._app.post_to_main_thread(self._win, self._draw_geometries_thread)
 
 
 class Interactive3DVisualizer:
@@ -36,9 +85,9 @@ def set_pointcloud_obj(pcd_obj, pcd_arr, color=None, tf=None):
     pose : (dict)
         LinSet Mesh (points, lines) f the camera view 5 points
     '''
-    pcd_obj.points = o3d.utility.Vector3dVector(pcd_arr)
+    pcd_obj.point["positions"] = np.float32(pcd_arr)
     if color is not None:
-        pcd_obj.paint_uniform_color(color)
+        pcd_obj.point["colors"] = np.float32([color]*len(pcd_arr))
     if tf is not None:
         pcd_obj.transform(tf)
 
@@ -50,35 +99,34 @@ def set_camera_view(lineset_obj, pose, color=[0, 1, 0], tf=None):
     pose : (dict)
         LinSet Mesh (points, lines) f the camera view 5 points
     '''
-    lineset_obj.points = o3d.utility.Vector3dVector(pose['points'])
-    lineset_obj.lines = o3d.utility.Vector2iVector(pose['lines'])
-    lineset_obj.paint_uniform_color(color)
+    lineset_obj.point["positions"] = np.float32(pose['points'])
+    lineset_obj.line["indices"] = np.float32(pose['lines'])
+    lineset_obj.line["colors"] = np.float32([color]*len(pose['lines']))
     if tf is not None:
         lineset_obj.transform(tf)
 
 
 if __name__ == '__main__':
-    o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
+    # o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Debug)
     img_to_pcd_tf = [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]
     kitti_src = "/home/loay/Documents/datasets/kitti/raw/odometry_gray/dataset"
-    kitti_odom = pykitti.odometry(kitti_src, "07")
-    pred_path = np.load('output/kitti-orb-mono-visual_odom.npz')['trajectory']
+    kitti_odom = pykitti.odometry(kitti_src, "03")
+    pred_path = np.load('output/kitti-orb-1000-mono-visual_odom.npz')['trajectory']
     gt_path = np.array([pose[:3, 3] for pose in kitti_odom.poses])
-    pcdObj1 = o3d.geometry.PointCloud()
-    pcdObj1.points = o3d.utility.Vector3dVector(np.array(gt_path))
-    pcdObj1.paint_uniform_color([0, 0, 1])
-    pcdObj1.transform(img_to_pcd_tf)
-    pcdObj2 = o3d.geometry.PointCloud()
-    vis = Interactive3DVisualizer("Odom", (0, 960), (960, 1080))
+    pcdObj1 = o3d.t.geometry.PointCloud()
+    set_pointcloud_obj(pcdObj1, gt_path, color=[0, 0, 1], tf=img_to_pcd_tf)
+    
+    pcdObj2 = o3d.t.geometry.PointCloud()
+    vis = Scenes3DVisualizer("Odom", (0, 960), (960, 1080))
 
     try:
         pred_traj = []
-        vis.add_geometries([pcdObj1, pcdObj2])
-        for i in range(0, 1000):
+        vis.add_geometries([{"name":"gt_path", "geometry": pcdObj1}])
+        for i in range(len(pred_path)):
             pred_traj.extend(pred_path[i:i+50])
-            pcdObj2.points = o3d.utility.Vector3dVector(np.array(pred_traj))
-            pcdObj2.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-            pcdObj2.paint_uniform_color([1, 0, 0])
+            set_pointcloud_obj(pcdObj2, pred_traj, color=[1, 0, 0], tf=img_to_pcd_tf)
+            vis.add_geometries([{"name":"pred_path", "geometry": pcdObj2}])
+            vis.show(interactive=False)
             # time.sleep(0.01)
-            vis.show(pcdObj2)
+        vis.close()
     except Exception as e: print(e)  
